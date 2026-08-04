e36941e3f3fa5bd6fcc085b350b8807f2699a61b
wasimmm3 <48721685+wasimmm3@users.noreply.github.com>
2026-08-05 01:13:32 +0300

    Add self-removing service worker to clear legacy caches

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>e.waitUntil(self.skipWaiting()));self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())await caches.delete(k);await self.registration.unregister();for(const c of await self.clients.matchAll({type:'window'}))c.navigate(c.url)})()));
